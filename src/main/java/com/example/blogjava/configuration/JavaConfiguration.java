4612c18997ba1642e4d77b96b0677d58226a2a58
shenshen <xiaoguaishou404@qq.com>
2023-05-27 17:35:08 +0800

    测试application.properties的乱码问题

--- src/main/java/com/example/blogjava/configuration/JavaConfiguration.java
+++ src/main/java/com/example/blogjava/configuration/JavaConfiguration.java
@@ -1,9 +1,14 @@
 package com.example.blogjava.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @Configuration
 public class JavaConfiguration {
+//    @Service
+//    @Component
+//    可以不写@Bean，使用上面两个注解的其中一个实现自动装配
 //    @Bean
 //    public OrderService orderService(UserService userService) {
 //        return new OrderService(userService);
--- src/test/java/com/example/blogjava/BlogJavaApplicationTests.java
+++ src/test/java/com/example/blogjava/BlogJavaApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.blogjava;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class BlogJavaApplicationTests {
+    @Autowired
+    private RedisTemplate redisTemplate;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void TestGet() {
+        Object name = redisTemplate.boundValueOps("name").get();
+        System.out.println(name);
+    }
+
+    @Test
+    public void TestSet() {
+        redisTemplate.boundValueOps("name").set("CodeGeeX");
+    }
+
+
+    @Test
+    void contextLoads() {
+    }
 
 }
